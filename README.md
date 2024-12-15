efb42282bd39558c073be7cc457e3b27bb3ffe21
Armen Deroian <armendero330@gmail.com>
2024-12-15 01:04:05 -0500

    add placing of network nodes

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Credits
 - Icon by [Pexelpy](https://freeicons.io/coworking-space-icon-set-31292/wifi-box-delivery-drone-package-icon-1137736) on [freeicons.io](https://freeicons.io)
+- Landing Pad Icon by [AbtoCreative](https://freeicons.io/traffic-and-transportation-51877/helipad-landing-pad-heliport-take-off-landing-icon-2215124) on [freeicons.io](https://freeicons.io)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+var start = false;
+var algorithm = null;
+var placeTarget = "drone-port";
 
-
-let start = false;
-let algorithm = null;
+var dronePorts = [];
 
 $(document).ready(function () {
     $("#start").click(function () {
@@ -10,4 +11,59 @@ $(document).ready(function () {
         console.log(algorithm);
         $(".menu").css("display", "none");
     });
+
+    $("#drone-port-select").css("background-color", "rgba(255, 255, 255, 0.25)")
+        .click(function (e) {
+            placeTarget = "drone-port";
+            $("#drone-port-select").css("background-color", "rgba(255, 255, 255, 0.25)");
+            $("#path-node-select").css("background-color", "rgba(0, 0, 0, 0)");
+        });
+
+    $("#path-node-select").click(function (e) {
+        placeTarget = "path-node";
+        $("#path-node-select").css("background-color", "rgba(255, 255, 255, 0.25)");
+        $("#drone-port-select").css("background-color", "rgba(0, 0, 0, 0)");
+    });
+
+    $(".map").click(function (e) {
+        if (start) {
+            if (placeTarget === "drone-port")
+                createDronePort(e.pageX - 16, e.pageY - 16);
+            else if (placeTarget === "path-node") {
+                createPathNode(e.pageX - 8, e.pageY - 8);
+            }
+        }
+    });
 });
+
+function createDronePort(x, y) {
+    if (!canCreateDronePort(x, y)) return;
+
+    $("#drone-port").clone(false, false).css("position", "absolute").css("left", x).css("top", y).css("z-index", 10).appendTo(".map");
+    dronePorts.push({ x: x, y: y });
+}
+
+function canCreateDronePort(x, y) {
+    for (var i = 0; i < dronePorts.length; i++) {
+        const portX = dronePorts[i].x;
+        const portY = dronePorts[i].y;
+
+        const bbX1 = portX - 200;
+        const bbX2 = portX + 200 + 32;
+        const bbY1 = portY - 200;
+        const bbY2 = portY + 200 + 32;
+
+        if (x >= bbX1 && x <= bbX2 && y >= bbY1 && y <= bbY2) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function createPathNode(x, y) {
+    $("#path-node").clone(false, false).css("position", "absolute").css("left", x).css("top", y).css("z-index", 10).appendTo(".map");
+}
+
+function canCreatePathNode(x, y) {
+    return true;
+}
